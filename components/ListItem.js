60c326081f00f9f27610411315e616d50a15f17b
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-07-01 18:51:32 +0100

    ddsds:  Please enter the commit message for your changes. Lines starting

---
+++ components/ListItem.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const ListItem = ({list, deleteItemFromStorage, navigation}) => {
+  //return our items alongside a delete icon that calls on deleteList function taking the element's id
+  return (
+    <TouchableOpacity
+      style={styles.ListItem}
+      onPress={() => navigation.navigate('Welcome View')}>
+      <View style={styles.ListItemView}>
+        <Text style={styles.listItemText}>{list.name}</Text>
+        <Icon
+          name="remove"
+          size={27}
+          color="firebrick"
+          onPress={() => deleteItemFromStorage(list.key)}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  ListItem: {
+    padding: 15,
+    paddingRight: 38,
+    borderBottomWidth: 1,
+  },
+  ListItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  listItemText: {
+    fontSize: 20,
+  },
+});
+
+export default ListItem;
---
+++ stylesheet.js
@@ -0,0 +1,55 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+const entireScreenWidth = Dimensions.get('window').width;
+const entireScreenHeight = Dimensions.get('window').height;
+const rem =
+  entireScreenWidth > entireScreenHeight
+    ? entireScreenHeight / 380
+    : entireScreenWidth / 380;
+EStyleSheet.build({$rem: rem});
+
+const styles = StyleSheet.create({
+  offlineStyle: {
+    flex: 1,
+  },
+  goOnlineButton: {
+    paddingBottom: EStyleSheet.value('50rem'),
+    alignSelf: 'center',
+    width: '50%',
+  },
+  inputContainer: {
+    padding: EStyleSheet.value('5rem'),
+  },
+  inputStyle: {
+    borderColor: 'black',
+    borderWidth: EStyleSheet.value('1rem'),
+    padding: EStyleSheet.value('10rem'),
+    borderRadius: EStyleSheet.value('2rem'),
+  },
+  manageTeamWrapper: {
+    width: EStyleSheet.value('350rem'),
+  },
+  manageTeamTitle: {
+    marginBottom: EStyleSheet.value('10rem'),
+  },
+  addTeamMemberInput: {
+    borderBottomColor: 'black',
+    borderBottomWidth: EStyleSheet.value('1rem'),
+    marginTop: EStyleSheet.value('5rem'),
+    fontSize: EStyleSheet.value('18rem'),
+  },
+  manageTeamButtonContainer: {
+    paddingTop: EStyleSheet.value('50rem'),
+    alignSelf: 'center',
+    width: '50%',
+    borderTopColor: 'grey',
+    borderBottomColor: 'grey',
+    borderBottomWidth: EStyleSheet.value('1rem'),
+  },
+  plusButton: {
+    fontSize: EStyleSheet.value('28rem'),
+    fontWeight: '400',
+  },
+});
+
+export default styles;
