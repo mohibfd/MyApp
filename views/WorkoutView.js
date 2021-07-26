b7df3522527576634b620a293264401c64c007ee
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-07-26 19:31:58 +0100

    made qr code into modal

--- views/WorkoutView.js
+++ views/WorkoutView.js
@@ -1,8 +1,18 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, Button, Image} from 'react-native';
+import {
+  SafeAreaView,
+  Button,
+  Image,
+  Dimensions,
+  Modal,
+  View,
+  TouchableHighlight,
+  Text,
+} from 'react-native';
 import uuid from 'react-native-uuid';
 import {launchImageLibrary} from 'react-native-image-picker';
 import ImagePicker from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import styles from '../stylesheets/stylesheet.js';
 import Header from '../components/Header';
@@ -10,6 +20,8 @@ import InvestItem from '../components/InvestItem';
 import DeleteOrCancel from '../components/DeleteOrCancel.js';
 
 const WorkoutView = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const [imagePicked, setImagePicked] = useState(null);
 
   console.log(imagePicked);
@@ -32,14 +44,42 @@ const WorkoutView = () => {
     <SafeAreaView style={styles.welcome}>
       <Header title="My Workouts" />
 
-      <Button onPress={() => test()} title="open gallery" color="gold" />
-
-      <Image
-        style={{width: 200, height: 200}}
-        source={{
-          uri: imagePicked,
-        }}
+      <Button
+        onPress={() => setModalVisible(true)}
+        title="QR Code"
+        color="black"
       />
+
+      <Modal
+        animationType="slide"
+        // transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}>
+        <Icon
+          style={styles.modalClose}
+          name="remove"
+          size={50}
+          color="red"
+          onPress={() => setModalVisible(false)}
+        />
+        <Button onPress={() => test()} title="open gallery" color="gold" />
+        <View
+          style={[styles.centeredView, {position: 'absolute', height: '100%'}]}>
+          {/* <View style={styles.modalView}></View> */}
+          <Image
+            style={{
+              alignSelf: 'center',
+              width: Dimensions.get('window').width,
+              height: Dimensions.get('window').width,
+            }}
+            source={{
+              uri: imagePicked,
+            }}
+          />
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
