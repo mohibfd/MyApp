d089d3a42a847c264429b5462b7ce8f08a364ef2
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-08-02 00:42:16 +0100

    adding new crypto prices get requests

--- components/modals/AddIconModal.js
+++ components/modals/AddIconModal.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
 });
 
 AddIconModal.propTypes = {
-  menuItems: PropTypes.object.isRequired,
+  menuItems: PropTypes.array.isRequired,
   addMainItem: PropTypes.func.isRequired,
   setModalVisible: PropTypes.func.isRequired,
 };
--- services/CryptoData.js
+++ services/CryptoData.js
@@ -9,11 +9,50 @@ export const getCryptoData = async () => {
       'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=gbp',
     );
 
+    const ripple = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price?ids=ripple&vs_currencies=gbp',
+    );
+
+    const binanceCoin = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=gbp',
+    );
+
+    const cardano = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=gbp',
+    );
+
+    // const matic-network = await axios.get(
+    //   'https://api.coingecko.com/api/v3/simple/price?ids=matic-network&vs_currencies=gbp',
+    // );
+
+    // const matic-network = await axios.get(
+    //   'https://api.coingecko.com/api/v3/simple/price?ids=matic-network&vs_currencies=gbp',
+    // );
+
+    // const matic-network = await axios.get(
+    //   'https://api.coingecko.com/api/v3/simple/price?ids=matic-network&vs_currencies=gbp',
+    // );
+
     const ETHPrice = ethereum.data.ethereum.gbp;
 
     const BTCPrice = bitcoin.data.bitcoin.gbp;
 
-    return [ETHPrice, BTCPrice];
+    const MSCIPrice = 77.55;
+
+    const XRPrice = ripple.data.ripple.gbp;
+
+    const BNBPrice = binanceCoin.data.binancecoin.gbp;
+
+    const ADAPrice = cardano.data.cardano.gbp;
+
+    // const ADAPrice = cardano.data.cardano.gbp;
+
+    // const ADAPrice = cardano.data.cardano.gbp;
+    // const ADAPrice = cardano.data.cardano.gbp;
+
+    console.log(ADAPrice);
+
+    return [ETHPrice, BTCPrice, MSCIPrice, XRPrice, BNBPrice, ADAPrice];
   } catch (error) {
     console.error(error.message);
   }
