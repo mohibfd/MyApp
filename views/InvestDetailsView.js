4c7c34f25b12a13cbd4e3f28db81c14191081999
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-08-08 10:55:08 +0100

    styling invest details page

--- views/InvestDetailsView.js
+++ views/InvestDetailsView.js
@@ -14,51 +14,16 @@ import Header from '../components/Header';
 const DeveloperDetailsView = ({route}) => {
   const investment = route.params.investment;
 
-  // investment.assets.map(asset => {
-  //   console.log(asset);
-  // });
-
-  let potato = {hi: 'test'};
-
-  potato.hey = 'tea';
-
-  console.log(potato);
-
   const Asset = ({name, photo, quantity, totalValue}) => (
-    <View
-      style={{
-        borderWidth: 1,
-        borderColor: myWhite,
-        flexDirection: 'row',
-        alignItems: 'center',
-      }}>
+    <View style={styles.container}>
       <View style={styles.imageContainer}>
         <Image style={styles.image} source={photo} />
       </View>
-      <Text
-        style={{
-          color: myWhite,
-          marginLeft: EStyleSheet.value('10rem'),
-          flex: 1,
-        }}>
-        {name}
-      </Text>
-      <Text
-        style={{
-          color: myWhite,
-          marginLeft: EStyleSheet.value('10rem'),
-          flex: 1,
-        }}>
+      <Text style={styles.text}>{name}</Text>
+      <Text style={[styles.text, {textAlign: 'center', marginRight: '5%'}]}>
         {quantity}
       </Text>
-      <Text
-        style={{
-          color: myWhite,
-          marginLeft: EStyleSheet.value('10rem'),
-          flex: 1,
-        }}>
-        {totalValue}
-      </Text>
+      <Text style={styles.text}>£{totalValue}</Text>
     </View>
   );
 
@@ -75,6 +40,24 @@ const DeveloperDetailsView = ({route}) => {
     <SafeAreaView style={{flex: 1, backgroundColor: myBlack}}>
       <Header title={investment.name} />
 
+      <View style={styles.headerContainer}>
+        <View style={styles.textContainer}>
+          <View style={styles.nameContainer}>
+            <Text style={[styles.textHeader, styles.textHeader1]}>Name</Text>
+          </View>
+        </View>
+        <View style={styles.textContainer}>
+          <View style={styles.nameContainer}>
+            <Text style={[styles.textHeader, styles.textHeader1]}>Amount</Text>
+          </View>
+        </View>
+        <View style={[styles.textContainer, styles.alignCenter]}>
+          <View style={styles.nameContainer}>
+            <Text style={[styles.textHeader, styles.textHeader1]}>Value</Text>
+          </View>
+        </View>
+      </View>
+
       <FlatList
         data={investment.assets}
         renderItem={renderItem}
@@ -85,15 +68,49 @@ const DeveloperDetailsView = ({route}) => {
 };
 
 const styles = StyleSheet.create({
-  imageContainer: {
-    // flex: 2.5,
+  container: {
+    borderWidth: 1,
+    borderColor: myWhite,
     flexDirection: 'row',
-    // justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flexDirection: 'row',
   },
   image: {
     width: '10%',
     height: '100%',
     padding: EStyleSheet.value('30rem'),
+  },
+  headerContainer: {
+    height: '7%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: '1%',
+  },
+  textContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  alignCenter: {
+    alignItems: 'center',
+  },
+  nameContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    borderLeftWidth: 2,
+    borderColor: myWhite,
+    paddingHorizontal: EStyleSheet.value('9rem'),
+  },
+  textHeader: {
+    color: myWhite,
+    fontSize: EStyleSheet.value('18rem'),
+  },
+  text: {
+    color: myWhite,
+    marginLeft: EStyleSheet.value('10rem'),
+    fontSize: EStyleSheet.value('18rem'),
+    flex: 1,
   },
 });
 
