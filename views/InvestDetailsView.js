307fe59e9848f151ce8a0e84d50ee5f39b11387f
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-08-09 18:10:17 +0100

    adding prop type

--- views/InvestDetailsView.js
+++ views/InvestDetailsView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   SafeAreaView,
   FlatList,
@@ -191,5 +192,9 @@ const styles = StyleSheet.create({
     marginRight: '5%',
   },
 });
+
+DeveloperDetailsView.propTypes = {
+  route: PropTypes.object.isRequired,
+};
 
 export default DeveloperDetailsView;
