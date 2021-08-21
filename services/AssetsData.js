34a0374c54256aead5a1f15b56a078b015b8fc4c
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-08-21 11:25:53 +0100

    changing msci price

--- services/AssetsData.js
+++ services/AssetsData.js
@@ -85,7 +85,7 @@ export const getAssetsData = async () => {
     const mostRecentPrice = mostRecentPriceList['1. open'];
 
     //we divide by 8 to make the price equal to fidelity's MSCI value
-    const MSCIFidelityPrice = mostRecentPrice / 8.03309805825;
+    const MSCIFidelityPrice = mostRecentPrice / 7.725;
 
     return [
       ETHPrice,
