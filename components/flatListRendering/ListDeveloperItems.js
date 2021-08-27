f405f64196a5ade2c2b205a972856e27e192b68c
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-08-28 00:52:21 +0100

    when a bug card is clicked it goes red now

--- components/flatListRendering/ListDeveloperItems.js
+++ components/flatListRendering/ListDeveloperItems.js
@@ -1,27 +1,33 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, Pressable, StyleSheet} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const ListDeveloperItems = ({item, deleteItemFromStorage}) => {
+  const [backgroundColor, setBackGroundColor] = useState(myBlue);
+
   //this will delete the item when the user clicks twice
   let temp = 0;
   const deletion = () => {
     temp += 1;
-    if (temp == 2) {
+    if (temp === 2 || backgroundColor === myRed) {
       deleteItemFromStorage(item);
+    } else {
+      setBackGroundColor(myRed);
     }
   };
 
   return (
-    <Pressable style={styles.container} onPress={() => deletion()}>
+    <Pressable
+      style={[styles.container, {backgroundColor}]}
+      onPress={() => deletion()}>
       <Text style={styles.developerText}>{item.name}</Text>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {borderBottomWidth: 2, backgroundColor: myBlue},
+  container: {borderBottomWidth: 2},
   developerText: {
     textAlign: 'center',
     fontSize: EStyleSheet.value('17rem'),
